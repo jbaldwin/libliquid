cd94944a547d05b893aaf4d06e1eccbf684c072b
jbaldwin <joshrbaldwin@gmail.com>
2018-12-09 11:30:00 -0700

    Test tolower lookup tables (they are faster!)

--- src/request/Request.cpp
+++ src/request/Request.cpp
@@ -20,13 +20,68 @@ static constexpr char HTTP_HTAB = '\t';
  * @param c
  * @return
  */
-auto tolower_ascii(int c) -> int
+inline auto tolower_ascii(int c) -> int
 {
     if(c >= 65 && c <= 90)
     {
         return c + 32;
     }
     return c;
+}
+
+inline auto tolower_ascii2(int c) -> int
+{
+    return c + ((static_cast<unsigned int>(c - 'A') < 26u) << 5);
+}
+
+static constexpr int tolower_table_add[] = {
+    0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,
+    32,32,32,32,32,32,32,32,32,32,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+};
+
+inline auto tolower_asciitable_add(int c) -> int
+{
+    return c + tolower_table_add[c];
+}
+
+static constexpr unsigned char tolower_table[] = {
+      0,
+      1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16,
+     17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
+     33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
+     49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64,
+     97, 98, 99,100,101,102,103,104,105,106,107,108,109,110,111,112,
+    113,114,115,116,117,118,119,120,121,122, 91, 92, 93, 94, 95, 96,
+     97, 98, 99,100,101,102,103,104,105,106,107,108,109,110,111,112,
+    113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,
+    129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,
+    145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,
+    161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,
+    177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,
+    193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,
+    209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,
+    225,226,227,228,229,230,231,232,233,234,235,236,237,238,239,240,
+    241,242,243,244,245,246,247,248,249,250,251,252,253,254,255
+};
+
+inline auto tolower_asciitable(int c) -> int
+{
+    return tolower_table[c];
 }
 
 /**
@@ -42,10 +97,14 @@ auto string_view_icompare(std::string_view a, std::string_view b) -> bool
         return false;
     }
 
-    for(size_t i = 0; i < a.length(); ++i)
+    const size_t len = a.length();
+    for(size_t i = 0; i < len; ++i)
     {
-        if(std::tolower(a[i]) != std::tolower(b[i])) // seems to be slightly faster O.O!
+//        if(std::tolower(a[i]) != std::tolower(b[i]))
 //        if(tolower_ascii(a[i]) != tolower_ascii(b[i]))
+//        if(tolower_ascii2(a[i]) != tolower_ascii2(b[i]))
+        if(tolower_asciitable_add(a[i]) != tolower_asciitable_add(b[i]))
+//        if(tolower_asciitable(a[i]) != tolower_asciitable(b[i]))
         {
             return false;
         }
@@ -538,14 +597,14 @@ auto Request::parseHeaders(std::string& data) -> ParseResult
         {
             auto& [name, value] = m_headers[m_header_count];
             if(
-                string_view_icompare(name, "Transfer-Encoding")
+                    string_view_icompare(name, "Transfer-Encoding")
                 &&  string_view_icompare(value, "chunked")
                 )
             {
                 m_body_type = BodyType::CHUNKED;
             }
             else if(
-                string_view_icompare(name, "Content-Length")
+                    string_view_icompare(name, "Content-Length")
                 &&  value.length() > 0
                 )
             {
